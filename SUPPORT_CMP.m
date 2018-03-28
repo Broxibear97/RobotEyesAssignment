3f2a1a7ef2acd5887d1352b322a2b57475617bb9
Broxibear97 <hogarthjoshie@aol.com>
2018-03-28 20:29:32 +0100

    Things work now?

--- SUPPORT_CMP.m
+++ SUPPORT_CMP.m
@@ -1,4 +1,4 @@
 % Write a function, SUPPORT_CMP(), comparing two support windows.
 function [difference] = SUPPORT_CMP(window1, window2)
-    difference = sad(window1, window2);
+    difference = ssd(window1, window2);
 end
