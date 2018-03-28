504ab1dec97a50f9a6b38e2fc327f6166685748b
Broxibear97 <hogarthjoshie@aol.com>
2018-03-28 21:30:05 +0100

    Updated

--- DISP_MAP.m
+++ DISP_MAP.m
@@ -1,44 +1,55 @@
-for i = 1:2
-
-    % prepare file name of next colour image to load
-    name = ...
-        strcat('test', ...
-            int2str(i) );
-    name = strcat(name, '.jpg');
-
-    f=imread(name);
-
-    % convert to monochrome by combining the RGB values 
-    % according to the NTSC standard
-    face = .2989*f(:,:,1)...
-        +.5870*f(:,:,2)...
-        +.1140*f(:,:,3);
-
-    % resize to make all imgs uniform (155x128).
-    % Some imgs in this set have a few rows or cols extra
-    [sr,sc] = size(face);
-    if sr > 240
-        face = face(1:240,:);  % if there are more than 155 rows, keep only the first 155
-    end
-
-    if sc > 320
-        face = face(:,1:320);  % if there are more than 128 columns, keep only the first 128
-    end
-
-    % save monochrome image
-    name = ...
-        strcat('bwtest', ...
-            int2str(i) );
-    name = strcat(name, '.jpg');
-
-    imwrite(face, name);
-
-end  % for loop ends
+% for i = 1:2
+% 
+%     % prepare file name of next colour image to load
+%     name = ...
+%         strcat('stereoPairs/pentagon_', ...
+%             int2str(i) );
+%     name = strcat(name, '.bmp');
+% 
+%     f=imread(name);
+% 
+%     % convert to monochrome by combining the RGB values 
+%     % according to the NTSC standard
+%     face = .2989*f(:,:,1)...
+%         +.5870*f(:,:,2)...
+%         +.1140*f(:,:,3);
+% 
+%     % resize to make all imgs uniform (155x128).
+%     % Some imgs in this set have a few rows or cols extra
+%     [sr,sc] = size(face);
+%     if sr > 240
+%         face = face(1:240,:);  % if there are more than 155 rows, keep only the first 155
+%     end
+% 
+%     if sc > 320
+%         face = face(:,1:320);  % if there are more than 128 columns, keep only the first 128
+%     end
+% 
+%     % save monochrome image
+%     name = ...
+%         strcat('bwtest', ...
+%             int2str(i) );
+%     name = strcat(name, '.jpg');
+% 
+%     imwrite(face, name);
+% 
+% end  % for loop ends
 
 % Disparity map stuff
-pLeft = imread('bwtest1.jpg');
-pRight = imread('bwtest2.jpg');
+% pLeft = rgb2gray(imread('stereoPairs\pentagon_1.bmp'));
+% pRight = rgb2gray(imread('stereoPairs\pentagon_2.bmp'));
 
+bw = 0;
+fileLeft = 'stereoPairs\pentagon_1.bmp';
+fileRight = 'stereoPairs\pentagon_2.bmp';
+
+if(bw == 0)
+    pLeft = imread(fileLeft);
+    pRight = imread(fileRight);
+else
+    pLeft = rgb2gray(imread(fileLeft));
+    pRight = rgb2gray(imread(fileRight));
+end
 % we want to loop through each pixel in the left image.
  maxRows = size(pLeft, 1);
  maxCols = size(pLeft, 2);
@@ -52,14 +63,6 @@ pRight = imread('bwtest2.jpg');
        %  disp("Pixel end!");
      end
  end
- 
+
 figure('Name', 'Clearly the better option');
 imshow(dispmap);
-
-figure('Name', 'Matlabs very own disparity map function'); 
-imshow(disparity(pLeft, pRight));
-
-
-
-
-
