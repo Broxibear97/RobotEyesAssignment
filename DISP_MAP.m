0205dd455abf5671efc1a7b6e7613c20ea34cbb2
Joshua Hogarth <hogarthjoshie@gmail.com>
2018-03-30 04:00:29 +0100

    updated master

--- DISP_MAP.m
+++ DISP_MAP.m
@@ -1,68 +1,31 @@
-% for i = 1:2
-% 
-%     % prepare file name of next colour image to load
-%     name = ...
-%         strcat('stereoPairs/pentagon_', ...
-%             int2str(i) );
-%     name = strcat(name, '.bmp');
-% 
-%     f=imread(name);
-% 
-%     % convert to monochrome by combining the RGB values 
-%     % according to the NTSC standard
-%     face = .2989*f(:,:,1)...
-%         +.5870*f(:,:,2)...
-%         +.1140*f(:,:,3);
-% 
-%     % resize to make all imgs uniform (155x128).
-%     % Some imgs in this set have a few rows or cols extra
-%     [sr,sc] = size(face);
-%     if sr > 240
-%         face = face(1:240,:);  % if there are more than 155 rows, keep only the first 155
-%     end
-% 
-%     if sc > 320
-%         face = face(:,1:320);  % if there are more than 128 columns, keep only the first 128
-%     end
-% 
-%     % save monochrome image
-%     name = ...
-%         strcat('bwtest', ...
-%             int2str(i) );
-%     name = strcat(name, '.jpg');
-% 
-%     imwrite(face, name);
-% 
-% end  % for loop ends
+function [disparity_map] = DISP_MAP(left_image_filename, right_image_filename,...
+    convert_to_bw, block_size, disparity_range)
 
-% Disparity map stuff
-% pLeft = rgb2gray(imread('stereoPairs\pentagon_1.bmp'));
-% pRight = rgb2gray(imread('stereoPairs\pentagon_2.bmp'));
+    if(convert_to_bw == 0)
+        p_left = imread(left_image_filename);
+        p_right = imread(right_image_filename);
+    else
+        p_left = rgb2gray(imread(left_image_filename));
+        p_right = rgb2gray(imread(right_image_filename));
+    end
 
-bw = 0;
-fileLeft = 'stereoPairs\pentagon_1.bmp';
-fileRight = 'stereoPairs\pentagon_2.bmp';
+    [max_cols, max_rows] = size(p_left);
+    disparity_map = zeros(size(p_left, 1), size(p_left, 2));
 
-if(bw == 0)
-    pLeft = imread(fileLeft);
-    pRight = imread(fileRight);
-else
-    pLeft = rgb2gray(imread(fileLeft));
-    pRight = rgb2gray(imread(fileRight));
+    % we want to loop through each pixel in the left image.
+    for col = 1 : max_cols
+        for row = 1 : max_rows 
+           [best_value, best_point, disparity_vector] = PIXEL_DISP(row, col, p_left(col, row), p_right, block_size, disparity_range);         
+           disparity_map(col, row) = best_value;
+           
+           % resulting image is probably really dark because i havent
+           % mapped the value between 0 and 255 yet.
+           % so it becomes either black, or white.
+           
+        end
+    end
+    
+    disp(disparity_map);
+    figure
+    imshow(disparity_map, []);
 end
-% we want to loop through each pixel in the left image.
- maxRows = size(pLeft, 1);
- maxCols = size(pLeft, 2);
- dispmap = zeros(size(pLeft, 1), size(pLeft, 2));
- 
- for row = 1: maxRows
-     for col = 1: maxCols
-        % disp("Pixel start");
-         [bestPoint, bestValue, disparityVector] = PIXEL_DISP(row, col, pLeft(row, col), pRight, 7);
-         dispmap(row, col) = abs(bestValue);
-       %  disp("Pixel end!");
-     end
- end
-
-figure('Name', 'Clearly the better option');
-imshow(dispmap);
--- ssd.m
+++ ssd.m
@@ -1,9 +1,3 @@
-function [SSD] = ssd(L, R)
-    intL = int16(L);
-    intR = int16(R);
-    SSDmatrix = (intL - intR);
-    
-    SSD = sumsqr(SSDmatrix);
-    
-    SSD = 0 - SSD;
-end
+function [SSD] = ssd(L, R)    
+    SSD = 0 - sumsqr(int16(L) - int16(R));
+ end
